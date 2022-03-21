ec1c5ce8bd26266d6c4350b72af0d01e822019a1
sanzh <aleksandrsan@mail.ru>
2022-03-21 09:48:43 +0300

    3.2 - done

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,22 +1,29 @@
 from rest_framework import serializers
 
+from .models import Product, StockProduct, Stock
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
 
     def create(self, validated_data):
+
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
 
@@ -26,8 +33,11 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            StockProduct(stock=stock, product=position['product'], quantity=position['quantity'], price=position['price']).save()
 
         return stock
+
 
     def update(self, instance, validated_data):
         # достаем связанные данные для других таблиц
@@ -39,5 +49,8 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        StockProduct.objects.filter(stock=stock).delete()
+        for position in positions:
+            StockProduct(stock=stock, product=position['product'], quantity=position['quantity'], price=position['price']).save()
 
         return stock
--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'logistic',
 ]
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'crud.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_stocks_products',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -129,3 +134,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 4
+}
